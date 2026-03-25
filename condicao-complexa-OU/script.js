1525bb3b7463c5ba1ca952d53b71b94acb217bde
DaviCordeiro2009 <Davimacielcordeiro2@gmail.com>
2026-03-25 08:35:41 -0300

    condicional utilizando o operador OR/OU

---
+++ condicao-complexa-OU/script.js
@@ -0,0 +1,10 @@
+//Declaração de variáveis e entrada de dados
+let idade = Number(prompt("Digite sua idade:"));
+let estudante = prompt("É estudante? (sim ou nao)");
+let professor = prompt("É professor? (sim ou nao)");
+//Condição com OU
+if (idade >= 60 || estudante === "sim" || professor) === "sim" {
+    alert("Você é maior de idade");
+} else {
+        alert("Não tem desconto");
+}
